204250520d06f385747dfdb4bf86e377fe5f9653
alamin500 <nalamin500@gmail.com>
2021-10-27 23:43:05 +0600

    h

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,12 +31,29 @@ async function run() {
       res.send(services);
     });
 
+    // Get Single Service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("id number", id);
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+
     // POST API
     app.post("/services", async (req, res) => {
       const service = req.body;
       console.log("hit the post api", service);
       const result = await servicesCollection.insertOne(service);
       console.log(result);
+      res.json(result);
+    });
+
+    // Delete Api
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
       res.json(result);
     });
   } finally {
