9c1402a9a2ac13ee6cb06f1371a0fab5f70d1c00
alamin500 <nalamin500@gmail.com>
2021-10-27 08:42:16 +0600

    h

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware;hi
 app.use(cors());
